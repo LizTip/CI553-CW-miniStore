795231b19eb9b83b6e60f38b985dcb32347438ae
LizTip <162799044+LizTip@users.noreply.github.com>
2024-10-21 11:42:52 +0100

    Improved version of Basket with merging and sorting functionality

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -2,19 +2,40 @@ package catalogue;
 
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Write a description of class BetterBasket here.
+ * Improved version of Basket with merging and sorting functionality
  * 
- * @author  Your Name 
+ * @author  Liz Tipper
  * @version 1.0
  */
-public class BetterBasket extends Basket implements Serializable
-{
+public class BetterBasket extends Basket implements Serializable {
   private static final long serialVersionUID = 1L;
-  
 
-  // You need to add code here
-  // merge the items for same product,
-  // or sort the item based on the product number
+  /**
+   * Merges items with the same product number in the basket
+   */
+  public void mergeItems() {
+    Map<String, Product> mergedProducts = new HashMap<>();
+    for (Product product : this) {
+      if (mergedProducts.containsKey(product.getProductNum())) {
+        Product existingProduct = mergedProducts.get(product.getProductNum());
+        existingProduct.setQuantity(existingProduct.getQuantity() + product.getQuantity());
+      } else {
+        mergedProducts.put(product.getProductNum(), product);
+      }
+    }
+    this.clear();
+    this.addAll(mergedProducts.values());
+  }
+
+  /**
+   * Sorts the basket items by product number
+   */
+  public void sortItemsByProductNumber() {
+    Collections.sort(this, Comparator.comparing(Product::getProductNum));
+  }
 }
